250763bd47755fcf1e464bdae664a89a46c80f6e
Daniel <danielcarlossantanna@gmail.com>
2022-02-13 09:21:22 -0300

    feature: add 404 error message

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, { Router, Request, Response } from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import * as dotenv from 'dotenv';
+import path from 'path';
+
 import connectDB from './config/database';
 
 // Load envs
@@ -32,4 +34,9 @@ const { PORT } = process.env;
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server running on port ${PORT}`);
+});
+
+app.use((req: Request, res: Response) => {
+  if (req.headers.accept?.includes('text/html')) res.status(404).sendFile(path.join(`${__dirname}/public/404.html`));
+  else res.status(404).send({ status: 'error', message: 'Page not found' });
 });
