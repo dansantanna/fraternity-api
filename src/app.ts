0521959d41b21e69ded6a65f953e966003fcb90d
Daniel <danielcarlossantanna@gmail.com>
2022-02-18 19:50:01 -0300

    feature: add apollo server

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 import * as dotenv from 'dotenv';
 import path from 'path';
 import swagger from 'swagger-ui-express';
+import { ApolloServer, gql } from 'apollo-server-express';
 
 import routes from 'routes';
 import connectDB from 'config/database';
@@ -28,17 +29,39 @@ app.use(cors());
 // Routes
 app.use(routes);
 
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+const graphqlPath = '/graphql';
+const server = new ApolloServer({ typeDefs, resolvers });
+server.start().then(() => {
+  server.applyMiddleware({ app, path: graphqlPath });
+});
+
 // Swagger
 app.use('/api-docs', swagger.serve, swagger.setup(swaggerDoc));
+
+app.use((req: Request, res: Response, next) => {
+  if (req.originalUrl !== graphqlPath) {
+    if (req.headers.accept?.includes('text/html'))
+      res.status(404).sendFile(path.join(`${__dirname}/public/404.html`));
+    else res.status(404).send({ status: 'error', message: 'Page not found' });
+  } else {
+    next();
+  }
+});
 
 const { PORT } = process.env;
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`server running on port ${PORT}`);
-});
-
-app.use((req: Request, res: Response) => {
-  if (req.headers.accept?.includes('text/html'))
-    res.status(404).sendFile(path.join(`${__dirname}/public/404.html`));
-  else res.status(404).send({ status: 'error', message: 'Page not found' });
+  console.log(`🚀 Server running on port ${PORT}`);
 });
