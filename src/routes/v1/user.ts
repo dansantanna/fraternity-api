b550e8f4ba0837985459954710f4e130ee753207
Daniel <danielcarlossantanna@gmail.com>
2022-03-04 13:06:51 -0300

    change password

--- src/routes/v1/user.ts
+++ src/routes/v1/user.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import { register } from 'controllers/user';
+import { register, changePassword } from 'controllers/user';
+import auth from 'middlewares/auth';
 
 const router = Router();
 
 router.post('/', register);
+router.post('/change-password', auth(), changePassword);
 
 export default router;
